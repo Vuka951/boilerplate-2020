180e09329f5c613c567546ab3294c5d886c4d25b
Vukasin Stojanovic <vultashin76@gmail.com>
2018-09-28 21:04:22 +0200

    Uses the data in saved in localStorage to "remember" the last lang u seleceted

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import {Provider} from 'react-redux';
 import {addLocaleData} from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import sr from 'react-intl/locale-data/sr';
+import {localeSet} from './actions/locale';
 import App from './components/App';
 import './main.css';
 
 addLocaleData(en);
 addLocaleData(sr);
+
+if (localStorage.boilerLang) {
+  store.dispatch(localeSet(localStorage.boilerLang));
+}
 
 ReactDOM.render(
   <Provider store={store}>
