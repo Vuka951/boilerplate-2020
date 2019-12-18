45c2dfae7a7b44368463a081655aa05a20d87288
Vuka951 <vultashin76@gmail.com>
2019-12-18 19:38:51 +0100

    react intl updated

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {store} from './store';
 import {Provider} from 'react-redux';
-import {addLocaleData} from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import sr from 'react-intl/locale-data/sr';
 import {localeSet} from './actions/locale';
 import App from './components/App';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-addLocaleData(en);
-addLocaleData(sr);
-
+// Loads the last selected lang on the site from local storage( if its in there)
 if (localStorage.boilerLang) {
   store.dispatch(localeSet(localStorage.boilerLang));
 }
